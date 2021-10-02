a07c128a033022945fb40c50a9a470425f606cfb
sayantan-bhattacharje <s.bhattacharjee00@gmail.com>
2021-10-03 00:25:36 +0530

    comming one after some basic installation to check the repo

---
+++ assesment/src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Dashboard.scss";
+
+const Dashboard = () => {
+  return <div className="Dashboard">This is DashBoard</div>;
+};
+
+export default Dashboard;
---
+++ assesment/src/Pages/PostPage/PostPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./PostPage.scss";
+
+const PostPage = () => {
+  return <div className="PostPage">This is PostPage</div>;
+};
+
+export default PostPage;
